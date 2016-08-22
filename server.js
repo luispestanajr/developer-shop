e3972cb720ab9245f119c337873070f6f16a3ecb
luis.pestanajr <luis.pestana@fsbrasil.com.br>
2016-08-22 03:03:02 -0300

    Ajustes para o deploy no heroku

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-'use strict';
-
 var http = require('http');
 var express = require('express');
 
 var app = express();
 var router = express.Router();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
 
 app.use(function(req, res, next){
-    // Website you wish to allow to connect
+	//Full access to all, it is very bad idea. Use this only for development
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     next();
 });
 
 app.get('/', function(req, res) {
-    res.sendfile('public/index.html');
+    //res.sendfile('public/index.html');
+    res.render('public/index')
 });
 
-//app.post('/enviar-contato', mail.enviaEmail);
-
-app.listen(80);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
